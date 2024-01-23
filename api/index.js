f5fe38c1a3403f31e3385977f513cae0c09a0947
tsi <tigbosi@gmail.com>
2024-01-23 21:43:05 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config;
+
+mongoose.connect(process.env.MONGO).then (() =>{
+
+}).catch((err) => {
+    console.log(err);
+
+})
+
+
+
 
 const app = express();
 
