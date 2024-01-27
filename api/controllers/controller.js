f23f06d095d8cbd22ff63211fb648e6f9db7b2ad
2nex <tigbosi@gmail.com>
2024-01-27 15:50:51 +0100

    create a middlewear function handler for possible errors

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,5 +1,5 @@
 export const test = (req, res) => {
     res.json({
-        message: "The Api is working very Well!",
+        message: 'The Api is working very Well!',
     });
 };
--- api/index.js
+++ api/index.js
@@ -3,26 +3,38 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
-dotenv.config;
+import authRouter from './routes/auth.route.js';
+dotenv.config();
 
 mongoose.connect(process.env.MONGO)
 .then (() =>{
-    console.log('Connect to MongoDb!')
+    console.log('Connected to MongoDb!')
 })
 .catch((err) => {
-
-    console.log(err);
-
-})
+ console.log(err);
+});
 
 
 
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
-    console.log('Server is running on port 30000');
+    console.log('Server is running on port 3000');
 }
 );
 
-app.use('/api/user', userRouter) 
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });  
+}) ;  
