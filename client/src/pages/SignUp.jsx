cc830af21d2a9801f723043eabfebb8fde9ce6dc
2nex <tigbosi@gmail.com>
2024-02-22 01:24:16 +0100

    Complete in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,11 +51,11 @@ setFormData({
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type='text' placedholder='username'
+        <input type='text' placeholder='username'
         className='border p-3 round-lg' id='username' onChange={handleChange}/>
-        <input type='email' placedholder='email'
+        <input type='email' placeholder='email'
         className='border p-3 round-lg' id='email'onChange={handleChange}/>
-        <input type='password' placedholder='password'
+        <input type='password' placeholder='password'
         className='border p-3 round-lg' id='password'onChange={handleChange}/>
         <button  disabled={loading} className='bg-slate-700 text-white p-3
         rounded-lg uppercase hover:opacity-95
@@ -65,7 +65,7 @@ setFormData({
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account</p>
-        <Link t0={'/sign-in'}>
+        <Link to={'/sign-in'}>
           <span className='text-blue-700'>Sign In</span>
 
         </Link>
